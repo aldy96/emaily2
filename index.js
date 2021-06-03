1847c5aae46e08787fc65ca5834c47269e0862d6
aldy96 <aldyfernando74@gmail.com>
2021-06-04 00:13:23 +0700

    baru

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express'); //pemanggilan modul di JS, tapi bukan ES15, contoh di ES15 = "import e from "e" "
+const express = require('express'); //pemanggilan modul di JS, tapi bukan ES15, contoh di ES15 = "import e from "e" " framework untuk mempermudah Protokol HTTP
+require('./services/passport'); //hanya di butuhkan berjalan saja no need to assign
+// const authRoutes = require('./routes/authRoutes'); bisa gini
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hello: 'bearly'});
-});
+// authRoutes(app); <== bisa gini
+require('./routes/authRoutes')(app);
+
+
+// app.get('/', (req, res) => { 
+//     res.send({hello: 'bearly'});
+// });
 //ada format tersendiri, get = command, / = router nya, req = reqques, res = respone
 
-const PORT = process.env.PORT || 5000;
+//app as an express, get is type of methode, "auth/google" as router, passport as callback
+
+
+const PORT = process.env.PORT || 5000; //The process.env global variable is injected by the Node at runtime for your application to use and it represents the state of the system environment your application is in when it starts
 app.listen(PORT); // pada port berapa
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+//untuk passport
+const passport = require('passport'); //library for handling authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //library from passport for specifies the authentication type provider example = "googleOauth"
+const keys = require("../config/keys")
+
+passport.use(
+    new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => { 
+        console.log('accesssToken:', accessToken);
+        console.log('refreshToken:',refreshToken);
+        console.log('profile:',profile);
+    }
+)); //telling the passport with use "hey passport you can handle auth, but this is the more specific way"
