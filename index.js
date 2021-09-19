60fae5293547dfea4354869f7b4670cd43ada75f
aldy96 <aldyfernando74@gmail.com>
2021-09-20 00:59:02 +0700

    abc

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const express = require('express'); //pemanggilan modul di JS, tapi bukan ES15, contoh di ES15 = "import e from "e" " framework untuk mempermudah Protokol HTTP
-// const mongoos = require('mongoose'); //calling mongoose to connect with mongodb
-// const cookieSession = require('cookie-session'); // for handling cookie and session
-// const passport = require('passport'); //we need it for handling cookies too
+const mongoos = require('mongoose'); //calling mongoose to connect with mongodb
+const cookieSession = require('cookie-session'); // for handling cookie and session
+const passport = require('passport'); //we need it for handling cookies too
 // const keys = require('./config/keys');
-// require('./models/User') // the code must be in order, call models user then passport, this argument for load Schema into mongoose
-// require('./services/passport'); //hanya di butuhkan berjalan saja no need to assign, this argument for fetch Schema from mongoose
+require('./models/User') // the code must be in order, call models user then passport, this argument for load Schema into mongoose
+require('./services/passport'); //hanya di butuhkan berjalan saja no need to assign, this argument for fetch Schema from mongoose
 // const authRoutes = require('./routes/authRoutes'); bisa gini
 
 
-// mongoos.connect(keys.mongoURI, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-// }) //settingan default dari petunjuk di Udemy, untuk koneksi ke mongoDB
+mongoos.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+}) //settingan default dari petunjuk di Udemy, untuk koneksi ke mongoDB
 
 const app = express();
 
-// //.use is function for mounting the middleware or function
-// app.use(
-//     cookieSession({
-//         maxAge: 30 * 24 * 60 * 60 * 1000, //for determine the last of our cookie in browser (30 days)
-//         keys: [keys.cookieKey], //this property is for encrypt the cookie
-//     })
-// ); 
+//.use is function for mounting the middleware or function
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //for determine the last of our cookie in browser (30 days)
+        keys: [keys.cookieKey], //this property is for encrypt the cookie
+    })
+); 
 
-// //telling passport to assign authentication process to cookies Session
-// app.use(passport.initialize());
-// app.use(passport.session());
+//telling passport to assign authentication process to cookies Session
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // authRoutes(app); <== bisa gini
-// require('./routes/authRoutes')(app);
+// authRoutes(app); <== bisa gini
+require('./routes/authRoutes')(app);
 
 
 app.get('/', (req, res) => { 
