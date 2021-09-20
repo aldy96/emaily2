a19affe797a4f7bfc38bc85159a2e205b2ce0314
aldy96 <aldyfernando74@gmail.com>
2021-09-21 00:31:25 +0700

    abcc

--- config/keys.js
+++ config/keys.js
@@ -8,8 +8,8 @@
 // }
 
 module.exports = {
-    googleClientID: '444107666856-ico3i9dq0tr6j10bcfomploo8kc3n8pu.apps.googleusercontent.com',
-    googleClientSecret: 'S3Q_1chEhbq-Zzc10Q07IEtC',
+    googleClientID: '7982080818-dagticjnbpgb768rj9atgqacmjdomkfv.apps.googleusercontent.com',
+    googleClientSecret: 'M20yz-maLrTvLg38xNZOk5gR',
     mongoURI: 'mongodb+srv://p00gz:XRiEhfUbrfrA3cI4@emaily.qrasn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     cookieKey: 'abc123',
 
