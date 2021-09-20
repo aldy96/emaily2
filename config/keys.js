3143bf26924345042249aa214db9a09315e29267
aldy96 <aldyfernando74@gmail.com>
2021-09-21 00:26:02 +0700

    abc

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,16 @@
-//keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === 'production') { //.env.NODE_ENV is heroku global variable for telling "this is production"
-    // we are in production - return the prod set of keys
-    module.export = require('./dev');
-} else {
-    // we are in development - return the dev keys
-    module.export = require('./dev');
-}
+// //keys.js - figure out what set of credentials to return
+// if (process.env.NODE_ENV === 'production') { //.env.NODE_ENV is heroku global variable for telling "this is production"
+//     // we are in production - return the prod set of keys
+//     module.export = require('./dev');
+// } else {
+//     // we are in development - return the dev keys
+//     module.export = require('./dev');
+// }
+
+module.exports = {
+    googleClientID: '444107666856-ico3i9dq0tr6j10bcfomploo8kc3n8pu.apps.googleusercontent.com',
+    googleClientSecret: 'S3Q_1chEhbq-Zzc10Q07IEtC',
+    mongoURI: 'mongodb+srv://p00gz:XRiEhfUbrfrA3cI4@emaily.qrasn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'abc123',
+
+};
--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@
 const passport = require('passport'); //library for handling authentication
 const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; //library from passport for specifies the authentication type provider example = "googleOauth"
-// const keys = require("../config/keys");***
+const keys = require('../config/keys')
 
 const User = mongoose.model('users'); //fetching the Schema, probably this is a class
 
@@ -18,31 +18,31 @@ passport.deserializeUser((id, done)=>{
     });
 });
 
-// passport.use(
-//     new GoogleStrategy( //function kemudian kita berikan clientID object sebagai parameter
-//     {
-//         clientID: keys.googleClientID,
-//         clientSecret: keys.googleClientSecret,
-//         callbackURL: '/auth/google/callback'
-//     }, 
+passport.use(
+    new GoogleStrategy( //function kemudian kita berikan clientID object sebagai parameter
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
 
-//     (accessToken, refreshToken, profile, done) => { 
-//         // untuk menambahkan new User({ googleId: profile.id }).save(); //assign data from google into Schema variable, probably this is an instance from class
-//         // "schema" findOne (for looking a record with same id with parameter (parameter = berisi variable key and value yang di assign) then promise arrow function
-//         User.findOne({ googleId: profile.id}).then(existingUser => {
-//             if(existingUser){
-//                 //we already have a record with the given id
-//                 done(null, existingUser);
-//             } else {
-//                 //we dont have a record with the given id
-//                 new User({ googleId: profile.id })
-//                 .save()
-//                 .then(user => {done(null, user)});
-//             }
-//         });
+    (accessToken, refreshToken, profile, done) => { 
+        // untuk menambahkan new User({ googleId: profile.id }).save(); //assign data from google into Schema variable, probably this is an instance from class
+        // "schema" findOne (for looking a record with same id with parameter (parameter = berisi variable key and value yang di assign) then promise arrow function
+        User.findOne({ googleId: profile.id}).then(existingUser => {
+            if(existingUser){
+                //we already have a record with the given id
+                done(null, existingUser);
+            } else {
+                //we dont have a record with the given id
+                new User({ googleId: profile.id })
+                .save()
+                .then(user => {done(null, user)});
+            }
+        });
         
-//         console.log('accesssToken:', accessToken);
-//         console.log('refreshToken:',refreshToken);
-//         console.log('profile:',profile);
-//     }
-// )); //telling the passport with use "hey passport you can handle auth, but this is the more specific ways"
+        console.log('accesssToken:', accessToken);
+        console.log('refreshToken:',refreshToken);
+        console.log('profile:',profile);
+    }
+)); //telling the passport with use "hey passport you can handle auth, but this is the more specific ways"
