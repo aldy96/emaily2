8dc74da83e63dc80eedbf189eeb6282f5b67c7c6
aldy96 <aldyfernando74@gmail.com>
2021-09-21 00:48:27 +0700

    nen

--- config/keys.js
+++ config/keys.js
@@ -1,15 +1,9 @@
 // //keys.js - figure out what set of credentials to return
-// if (process.env.NODE_ENV === 'production') { //.env.NODE_ENV is heroku global variable for telling "this is production"
-//     // we are in production - return the prod set of keys
-//     module.export = require('./dev');
-// } else {
-//     // we are in development - return the dev keys
-//     module.export = require('./dev');
-// }
-
-module.exports={
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
+if (process.env.NODE_ENV === 'production') { //.env.NODE_ENV is heroku global variable for telling "this is production"
+    // we are in production - return the prod set of keys
+    module.export = require('./prod.js');
+} else {
+    // we are in development - return the dev keys
+    module.export = require('./dev.js');
 }
+
