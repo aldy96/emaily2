9b67f5a53d4d2ed103e6b7416786428456f64845
aldy96 <aldyfernando74@gmail.com>
2021-09-21 00:19:24 +0700

    new

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dont commit this!!!
+
+module.exports = {
+    googleClientID: '444107666856-ico3i9dq0tr6j10bcfomploo8kc3n8pu.apps.googleusercontent.com',
+    googleClientSecret: 'S3Q_1chEhbq-Zzc10Q07IEtC',
+    mongoURI: 'mongodb+srv://p00gz:XRiEhfUbrfrA3cI4@emaily.qrasn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'abc123',
+
+};
