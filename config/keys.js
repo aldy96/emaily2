9885b5cc43f8b8caaf94e2198db29c14d61aab23
aldy96 <aldyfernando74@gmail.com>
2021-09-21 00:51:58 +0700

    d

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 // //keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') { //.env.NODE_ENV is heroku global variable for telling "this is production"
     // we are in production - return the prod set of keys
-    module.export = require('./prod.js');
+    module.exports = require('./prod.js');
 } else {
     // we are in development - return the dev keys
-    module.export = require('./dev.js');
+    module.exports = require('./dev.js');
 }
 
