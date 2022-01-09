1db7d60ed1c6322c8ecd368d103c8a32709bcf88
aldy96 <aldyfernando74@gmail.com>
2022-01-10 01:23:19 +0700

    New

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// dont commit this!!!
+
+module.exports = {
+    googleClientID: '444107666856-ico3i9dq0tr6j10bcfomploo8kc3n8pu.apps.googleusercontent.com',
+    googleClientSecret: 'S3Q_1chEhbq-Zzc10Q07IEtC',
+    mongoURI: 'mongodb+srv://p00gz:XRiEhfUbrfrA3cI4@emaily.qrasn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'abc123',
+    stripePublishableKey:'pk_test_51JxolfCI3CL1RYIJRkU7tGiHhcXQ6bJPIwWHplsHq6gAMjt4m3wXTAnUcdmcqo1kmsEV8X9m99UhjCRDgP4vpjKf006Xvk7CVu',
+    stripeSecretKey:'sk_test_51JxolfCI3CL1RYIJLMSpYJG0aqc0moKpXf87XNdZJY6AV395jHQe83jopgOR7RKoUX3yG0mUlhZCVfU21meG5MyF00NCZP7ROZ',
+
+};
